7110b2fd908dc64ab0e267c53dc0a1125b052c79
Alexander Ortiz <aorti017@ucr.edu>
2015-08-19 10:45:08 -0700

    moved training data and created main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+import os, string, codecs
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+
+genreTrain = ["./res/train/hiphop"]
+
+lyrics = []
+genre = []
+
+stemmer = SnowballStemmer("english")
+for genrePath in genreTrain:
+	for subdir, dirs, files in os.walk(genrePath):
+		for f in files:
+			print f
+			temp = ""
+			pth = os.path.join(subdir, f)
+			fileLyrics = open(pth).read().decode('utf-8')
+			fileLyrics = fileLyrics.replace(string.punctuation, "")
+			for i in string.punctuation:
+				fileLyrics = fileLyrics.replace(i, '')
+			for i in fileLyrics.split(" "):
+				temp += stemmer.stem(i) + " "
+			lyrics.append(temp)
+			if genrePath == genreTrain[0]:
+				genre.append("HH")
+			else:
+				genre.append("SG")
+				
+
+sw = stopwords.words("english")
+
+countVec = CountVectorizer(stop_words=sw)
+trainCount = countVec.fit_transform(lyrics)
+
+classifier = MultinomialNB(alpha=.5).fit(trainCount, genre)
+
+#testCount = countVec.transform(testSet)
+#predicted = classifier.predict(testCount)
+#print "Accuracy: " + str(accuracy_score(predicted, predTest))
