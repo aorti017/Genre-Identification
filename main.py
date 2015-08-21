019a3243bc1abcc0bc6e9f12f9becd314f523d08
Alexander Ortiz <aorti017@ucr.edu>
2015-08-21 11:15:22 -0700

    added tfidf accuracy ratings

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env
 import os, string, codecs
 from sklearn import svm, tree
 from sklearn.naive_bayes import MultinomialNB, GaussianNB
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.metrics import accuracy_score
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
@@ -55,6 +56,28 @@ sw = stopwords.words("english")
 countVec = CountVectorizer(stop_words=sw)
 trainCount = countVec.fit_transform(lyrics)
 testCount = countVec.transform(testSet)
+
+print "Using Count Vectorizer\n----------"
+clfMNB = MultinomialNB(alpha=.5).fit(trainCount, genre)
+predicted = clfMNB.predict(testCount)
+print "Multinomial Naive Bayes Accuracy: " + str(round(accuracy_score(predicted, predTest), 3))
+
+clfGNB = GaussianNB().fit(trainCount.toarray(), genre)
+predicted = clfGNB.predict(testCount.toarray())
+print "Gaussian Naive Bayes Accuracy: " + str(round(accuracy_score(predicted, predTest), 3))
+
+clfSVM = svm.SVC().fit(trainCount, genre)
+predicted = clfSVM.predict(testCount)
+print "SVM Accuracy: " + str(round(accuracy_score(predicted, predTest), 3))
+
+clfDT = tree.DecisionTreeClassifier().fit(trainCount.toarray(), genre)
+predicted = clfDT.predict(testCount.toarray())
+print "Decision Tree Accuracy: " + str(round(accuracy_score(predicted, predTest), 3))
+
+print "\nUsing TF-IDF Vectorizer\n----------"
+tfidfVec = TfidfTransformer().fit(trainCount)
+trainCount = tfidfVec.transform(trainCount)
+testCount = tfidfVec.transform(testCount)
 
 clfMNB = MultinomialNB(alpha=.5).fit(trainCount, genre)
 predicted = clfMNB.predict(testCount)
